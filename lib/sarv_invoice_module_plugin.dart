1572b5172d133368a2d75d8460b946f20c966254
Mehran <mehran@Mehrans-MacBook-Pro.local>
2022-12-19 21:11:09 +0330

    add di

--- lib/sarv_invoice_module_plugin.dart
+++ lib/sarv_invoice_module_plugin.dart
@@ -1,15 +1,35 @@
 library sarv_invoice_module_plugin;
 
+import 'core/injection/di.dart' as di;
+
 import 'package:flutter/material.dart';
 import 'package:sarv_invoice_module_plugin/features/invoice/presentation/pages/sell/new_sell_invoice_page.dart';
 
-class NewSellInvoiceModule extends StatelessWidget {
+
+
+class NewSellInvoiceModule extends StatefulWidget {
   const NewSellInvoiceModule({
     Key? key,
     required this.isPOS,
   }) : super(key: key);
   final bool isPOS;
-  
+
   @override
-  Widget build(BuildContext context) => NewSellInvoicePage();
+  State<NewSellInvoiceModule> createState() => _NewSellInvoiceModuleState();
+}
+
+class _NewSellInvoiceModuleState extends State<NewSellInvoiceModule> {
+  @override
+  void initState() {
+    super.initState();
+    di.configureDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Text(widget.isPOS.toString()),
+          Expanded(child: NewSellInvoicePage()),
+        ],
+      );
 }
